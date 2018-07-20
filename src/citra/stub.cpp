9c2c40e1f8447d8a156b80c5f0b7e272d314aa32
CaptV0rt3x <c.vamsi.krishna.96@gmail.com>
2018-07-20 17:30:33 +0530

    Minor Fixes

--- src/citra/stub.cpp
+++ src/citra/stub.cpp
@@ -3,13 +3,15 @@
 // Refer to the license.txt file included.
 
 #define SDL_MAIN_HANDLED
-#include <SDL2/SDL.h>
+#include <SDL.h>
 
 int main(int argc, char* argv[]) {
 #ifdef _WIN32
-    auto msg = "\"citra.exe\" has been renamed to \"citra-cmd.exe\". Please update your shortcuts!";
+    auto msg = "\"citra.exe\" has been renamed to \"citra-cmd.exe\". Please update your shortcuts, "
+               "and delete \"citra.exe\" after renaming";
 #else
-    auto msg = "\"citra\" has been renamed to \"citra-cmd\". Please update your shortcuts!";
+    auto msg = "\"citra\" has been renamed to \"citra-cmd\". Please update your shortcuts, and "
+               "delete \"citra\" after renaming";
 #endif
     SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, "Citra - ERROR", msg, nullptr);
     return 0;
